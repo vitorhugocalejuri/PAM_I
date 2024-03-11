48c8f39869dccb6296275ea5e7f9a48c9a78f3a0
unknown <vitor.silva1160@etec.sp.gov.br>
2024-03-11 20:55:40 -0300

    branch com o código da aula do dia Mon, Mar 11, 2024  8:55:39 PM

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu App"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(12),
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Atividade '),
+              Text('Avaliativa'),
+              Column(
+                children: [
+                  Text('Nome: Vitor Hugo'),
+                  Text('Email: vinicius.oliveira753.etec.sp.gov.br'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(const Aplicacao());
